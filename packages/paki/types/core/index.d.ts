e9976179ce6e290e1fdf52148a3d46e690e244b1
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-03-20 11:59:32 +0800

    Update index.d.ts

--- packages/paki/types/core/index.d.ts
+++ packages/paki/types/core/index.d.ts
@@ -1,8 +1,8 @@
 declare type targetType = "number" | "string" | "array" | "object" | "function";
 interface Plugin {
-  target: targetType;
-  name: string;
-  func: Function;
+    target: targetType;
+    name: string;
+    func: Function;
 }
 export declare function extend(plugin: Plugin): void;
 export {};
